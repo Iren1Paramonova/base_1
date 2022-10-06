77effe7043bb27c725f59c2300f5891eea478066
Iren1Paramonova <zerber17@ya.ru>
2022-10-06 21:15:41 +0300

    классная работа 2

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-console.log("Hello Irina");
+/*console.log("Hello Irina");
 const ourPlanetName = "Земля";
 console.log(ourPlanetName);
 let currentTime = "12:06";
 console.log(currentTime);
+
 let numberOfArticle = "3";
+
 console.log(numberOfArticle);
 let paymentMethod = "это оплата наличными деньгами или нет";
 console.log(paymentMethod);
-const userLastName = 'Парамонова';
+const userLastName = "Парамонова";
 console.log(userLastName);
-const userName = 'Ирина';
+const userName = "Ирина";
 console.log(userName);
-const userPatronymic = 'Альбертовна';
+const userPatronymic = "Альбертовна";
 console.log(userPatronymic);
+
+const userData = `Этот код написала ${userName}`;
+console.log(userData);
+
+let townName;
+console.log(townName);
+
+//const isUser = false;
+const isVisible = true;*/
+
+const userData = {
+  name: "Ivan",
+  age: 34,
+};
+console.log(typeof userData.age);
