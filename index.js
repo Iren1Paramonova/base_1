329571d08d26306fb63401a7f1500f33e67eefc2
Iren1Paramonova <zerber17@ya.ru>
2022-10-06 19:22:35 +0300

    добавила файлы первого занятия

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello Irina");
+const ourPlanetName = "Земля";
+console.log(ourPlanetName);
+let currentTime = "12:06";
+console.log(currentTime);
+let numberOfArticle = "3";
+console.log(numberOfArticle);
+let paymentMethod = "это оплата наличными деньгами или нет";
+console.log(paymentMethod);
+const userLastName = 'Парамонова';
+console.log(userLastName);
+const userName = 'Ирина';
+console.log(userName);
+const userPatronymic = 'Альбертовна';
+console.log(userPatronymic);
