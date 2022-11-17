108b35c7b33dcd681522eb9b5c67cbad0ab2e492
Iren1Paramonova <zerber17@ya.ru>
2022-11-17 21:01:13 +0300

    17/11

--- lesson9.js
+++ lesson9.js
@@ -20,26 +20,14 @@ const testObj = {
 };
 testObj.exampleMetod(); */
 
-const persona = {
+/* const persona = {
   name: "Ira",
   age: 5,
   getName: function () {
     console.log(this);
   },
 };
-persona.getName();
-
-/*let obj = {
-  name: "NN",
-  age: 802,
-  cize: 1000000,
-  getName: function () {
-    console.log(this.age);
-    return this.name;
-  },
-};
-console.log();
- */
+persona.getName(); */
 
 /*function example() {
   console.log(this);
@@ -51,3 +39,41 @@ example(); */
 }
 const testObject = { testMethod: example };
 testObject.testMethod(); */
+
+/* const town = {
+  name: "Moscow",
+  population: "25 mln",
+  getName: function () {
+    console.log(this.name);
+    return this.name;
+  },
+  getPopulation: function () {
+    console.log(this.population);
+    return this.population;
+  },
+  setAnyValue: function (key, value) {
+    this[key] = value;
+  },
+};
+town.getPopulation();
+town.setAnyValue("name", "London");
+town.setAnyValue("country", "UK");
+town.getName();
+console.log(town); */
+
+const user1 = { name: "Vasya", surName: "Ivanov" };
+const user2 = { name: "Petya", surName: "Semenov" };
+function greeting(phrase, smile) {
+  console.log(`${this.name} ${this.surName} : ${phrase} : ${smile}`);
+}
+greeting.call(user1, "Hello", "O_o");
+greeting.call(user2, "Privet", "^_^");
+
+const greetingUser1 = greeting.bind(user1, "Yuu", "))");
+greetingUser1();
+
+const greetingUser2 = greeting.bind(user2);
+greetingUser2("kuku", "(((");
+
+const greetingUser3 = greeting.bind(user2, "kuku");
+greetingUser3("(((");
