46a8981b56f8c97953a1c8838ce5fa10b3af472d
Iren1Paramonova <zerber17@ya.ru>
2022-11-21 21:20:01 +0300

    21/11

--- lesson10.js
+++ lesson10.js
@@ -12,7 +12,7 @@ console.log(second(10, 10));
 console.log(second(2, 5));
 console.log(second(5, 7)); */
 
-const cubeCarryValue = (a) => {
+/* const cubeCarryValue = (a) => {
   return (b) => {
     return (c) => {
       return a * b * c;
@@ -29,3 +29,53 @@ const twoArgs = cubeCarryValue(1)(3);
 console.log(twoArgs(3));
 console.log(twoArgs(4));
 console.log(twoArgs(5));
+ */
+
+//прототипы
+
+/* const russian = {
+  country: "Russia",
+  city: "NN",
+  __proto__: null, //прекращение цепи наследования
+};
+const people = {
+  name: "Ira",
+  age: 32,
+  __proto__: russian,
+};
+console.log(people.city, people);
+ */
+/* const obj = {
+  city: "NN",
+};
+const russian = {
+  country: "Russia",
+  __proto__: obj,
+};
+const people = {
+  name: "Ira",
+  age: 32,
+  __proto__: russian,
+};
+console.log(people.city); */
+
+//функции-конструкторы
+
+/* const userName = "Ira";
+const user = {
+  userName,
+};
+console.log(user); */
+
+function CreateUser(userName, userAge) {
+  this.name = userName;
+  this.age = userAge;
+  this.sayHi = function () {
+    console.log(`Привет, меня зовут ${this.name}`);
+  };
+}
+const firstUser = new CreateUser("Alex", 28);
+const secondUser = new CreateUser("Tony", 24);
+const thirdUser = new CreateUser("Ivan", 15);
+console.log(firstUser, secondUser, thirdUser);
+secondUser.sayHi();
